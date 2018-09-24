607965cec8cf8fc0d4aa885f45d94557ba53b67a
Yash Bhavsar <yashbhavsar.yb@gmail.com>
2018-09-24 18:56:53 +0530

    Changes in validate function
    Bug fixes of validation function

--- tokentime.cpp
+++ tokentime.cpp
@@ -1,5 +1,5 @@
 /**
- * Created By @yashbhavsar007
+ * Created By @yashBhavsar007
  * 
  */
 
@@ -8,16 +8,20 @@
 
 namespace eosio {
 
+// Token creation method
 void token::create( account_name issuer,
                     asset        maximum_supply )
 {
-    require_auth( _self );
+    require_auth( _self );// requires authorization of contract owner
     usertable userstats(_self, _self);
+    // adding account name to userstats table
     userstats.emplace(_self, [&](auto &p) {
             p.account = issuer;
             p.time_limit = 0;
+            
     });
     auto sym = maximum_supply.symbol;
+    // checking for validation
     eosio_assert( sym.is_valid(), "invalid symbol name" );
     eosio_assert( maximum_supply.is_valid(), "invalid supply");
     eosio_assert( maximum_supply.amount > 0, "max-supply must be positive");
@@ -26,6 +30,7 @@ void token::create( account_name issuer,
     auto existing = statstable.find( sym.name() );
     eosio_assert( existing == statstable.end(), "token with symbol already exists" );
 
+    // adding currency record to stats table
     statstable.emplace( _self, [&]( auto& s ) {
        s.supply.symbol = maximum_supply.symbol;
        s.max_supply    = maximum_supply;
@@ -33,15 +38,13 @@ void token::create( account_name issuer,
     });
 }
 
-
+// issuing method
 void token::issue( account_name to, asset quantity, string memo )
 {
     auto sym = quantity.symbol;
+    // validating asset
     eosio_assert( sym.is_valid(), "invalid symbol name" );
     eosio_assert( memo.size() <= 256, "memo has more than 256 bytes" );
-
-    
-
 
     auto sym_name = sym.name();
     stats statstable( _self, sym_name );
@@ -49,59 +52,85 @@ void token::issue( account_name to, asset quantity, string memo )
     eosio_assert( existing != statstable.end(), "token with symbol does not exist, create token before issue" );
     const auto& st = *existing;
 
-    require_auth( st.issuer );
+    require_auth( st.issuer ); // requires authorisation of issuer
     eosio_assert( quantity.is_valid(), "invalid quantity" );
     eosio_assert( quantity.amount > 0, "must issue positive quantity" );
 
     eosio_assert( quantity.symbol == st.supply.symbol, "symbol precision mismatch" );
     eosio_assert( quantity.amount <= st.max_supply.amount - st.supply.amount, "quantity exceeds available supply");
 
+    // modofying supply ammount
     statstable.modify( st, 0, [&]( auto& s ) {
        s.supply += quantity;
     });
-
+     // adding balance 
     add_balance( st.issuer, quantity, st.issuer );
 
+    // calling inline action to transfer token
     if( to != st.issuer ) {
        SEND_INLINE_ACTION( *this, transfer, {st.issuer,N(active)}, {st.issuer, to, quantity, memo} );
     }
 }
 
-void token::validate( account_name name)
+// Function to validate user 
+void token::validate( account_name name,account_name to)
 {
     usertable userstats(_self, _self);
     auto itr = userstats.find(name);
-    if(name == _self)
+    auto itr1 = userstats.find(to);
+    // if user is admin then it just print hi admin
+    if(name == _self ) 
     {
-        print("hi admin!");
+        print("hi admin!"); 
+        if(itr1 == userstats.end())
+        {
+            userstats.emplace(_self, [&](auto &p) {
+                p.account = to;
+                p.time_limit = now() + 100;
+                
+            });
+        }
     }
-    else if(name != _self && itr == userstats.end())
+    // if user is in not in our record then it adds user in recrd
+    else if(name != _self && itr == userstats.end() )
+    
     {
+       
         userstats.emplace(_self, [&](auto &p) {
             p.account = name;
-            p.time_limit = now() + 86400;
+            p.time_limit = now() + 100;
+            
         });
-        print("in issue");
+        print("in validate");
     }
-    else{
-        userstats.modify(itr ,0, [&](auto &p) {
-            p.time_limit = now() + 86400;
-
-        });
-    }
+    else if(itr1 == userstats.end())
+        {
+            userstats.emplace(_self, [&](auto &p) {
+                p.account = to;
+                p.time_limit = 0;
+                
+            });
+        }
+    
 }
+
+// transfer function for users to transfer token between two accounts
 void token::transfer( account_name from,
                       account_name to,
                       asset        quantity,
                       string       memo )
 {  
-    
-    validate(from); 
-    usertable userstats(_self, _self);
+    validate(from,to);
+    usertable userstats(_self, _self); // using user stats table
     auto itr = userstats.find(from); 
     eosio_assert(itr != userstats.end(), "Account does not match with our records in transfer "); 
+
+   
+    
+    // checking if current time is greater than user's time limit in our record 
     if(itr->time_limit < now())
     {
+        // validating user
         eosio_assert( from != to, "cannot transfer to self" );
         require_auth( from );
         eosio_assert( is_account( to ), "to account does not exist");
@@ -118,18 +147,21 @@ void token::transfer( account_name from,
         eosio_assert( memo.size() <= 256, "memo has more than 256 bytes" );
 
 
-        sub_balance( from, quantity );
-        add_balance( to, quantity, from );
+        sub_balance( from, quantity ); // calling sub_balance to decrease amount of from 
+        add_balance( to, quantity, from ); // calling add_balance to increase ammount of to
 
     }
+    // if user's timelimit is greater than now then it exicutes else part and gives assertion
     else
     {
-        eosio_assert(false,"Your Token is not unlocked yet! Wait untill it unlock!!!");
+        eosio_assert(false,"Your Token is not unlocked yet! Wait untill it unlock!!!"); 
     }
+   
 
     
 }
 
+// sub_balance to decrease balance function provided by eosio
 void token::sub_balance( account_name owner, asset value ) {
    accounts from_acnts( _self, owner );
 
@@ -145,7 +177,7 @@ void token::sub_balance( account_name owner, asset value ) {
       });
    }
 }
-
+ // add_balance to increase balance function provided by eosio
 void token::add_balance( account_name owner, asset value, account_name ram_payer )
 {
    accounts to_acnts( _self, owner );
